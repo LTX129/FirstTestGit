e428c00838a201a9711c0d43c3f6ca4d47f35845
Li <21722052@bjtu.edu.cn>
2023-10-24 14:59:55 +0800

    现在能测lancaster.ac.uk以及www.canva.com,能算出TTL了，但是还是测不了百度

--- TestPing.py
+++ TestPing.py
@@ -41,9 +41,11 @@ def receiveOnePing(icmpSocket, destinationAddress, ID, timeout):
     try:
         data, addr = icmpSocket.recvfrom(1024)
         time_received = time.time() * 1000  # Convert to milliseconds
-        return data, time_received
+        # Extract TTL from the IP header (the 9th byte)
+        ttl = struct.unpack('B', data[8:9])[0]
+        return data, time_received, ttl
     except socket.timeout:
-        return None, None
+        return None, None, None
 
 
 def sendOnePing(icmpSocket, destinationAddress, ID):
@@ -77,7 +79,7 @@ def doOnePing(destinationAddress, timeout):
         time_sent = sendOnePing(icmpSocket, destinationAddress, ID)
 
         # Call receiveOnePing function
-        response, time_received = receiveOnePing(icmpSocket, destinationAddress, ID, timeout)
+        response, time_received, ttl = receiveOnePing(icmpSocket, destinationAddress, ID, timeout)
 
         # Close ICMP socket
         icmpSocket.close()
@@ -85,13 +87,13 @@ def doOnePing(destinationAddress, timeout):
         if response is not None and time_received is not None:
             # Calculate total network delay
             network_delay = time_received - time_sent
-            return network_delay
+            return network_delay, ttl
         else:
-            return None
+            return None, None
 
     except socket.error as e:
         print(f"Socket error: {e}")
-        return None
+        return None, None
 
 
 def ping(host, timeout=1, count=4):
@@ -107,12 +109,12 @@ def ping(host, timeout=1, count=4):
         for _ in range(count):
             # Call doOnePing function, approximately every second
             network_delay = doOnePing(destinationAddress, timeout)
-
+            network_delay, ttl = doOnePing(destinationAddress, timeout)
             if network_delay is not None:
                 packets_sent += 1
                 packets_received += 1
                 total_delay += network_delay
-                print(f"Reply from {destinationAddress}: bytes=32 time={int(network_delay)}ms TTL=50")
+                print(f"Reply from {destinationAddress}: bytes=32 time={int(network_delay)}ms TTL={ttl}")
                 time.sleep(1)  # Wait for 1 second before sending the next ping
             else:
                 print("Request timed out.")
@@ -134,4 +136,4 @@ def ping(host, timeout=1, count=4):
 
 
 # Example usage:
-ping("lancaster.ac.uk")
+ping("www.canva.com")
