571f0e2491c3ac405eef30a0ea3a6172169300e0
Li <21722052@bjtu.edu.cn>
2023-10-24 15:45:08 +0800

    全新的代码，依然只能测两个网站

--- TestPing.py
+++ TestPing.py
@@ -5,6 +5,8 @@ import struct
 import time
 import select
 import binascii
+import threading
+import array
 
 ICMP_ECHO_REQUEST = 8  # ICMP type code for echo request messages
 ICMP_ECHO_REPLY = 0  # ICMP type code for echo reply messages
@@ -35,104 +37,66 @@ def checksum(string):
     return answer
 
 
-def receiveOnePing(icmpSocket, destinationAddress, ID, timeout):
-    # Wait for the socket to receive a reply
-    icmpSocket.settimeout(timeout)
-    try:
-        data, addr = icmpSocket.recvfrom(1024)
-        time_received = time.time() * 1000  # Convert to milliseconds
-        # Extract TTL from the IP header (the 9th byte)
-        ttl = struct.unpack('B', data[8:9])[0]
-        return data, time_received, ttl
-    except socket.timeout:
-        return None, None, None
-
-
-def sendOnePing(icmpSocket, destinationAddress, ID):
-    # Build ICMP header
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, 0, ID, 1)
-    data = struct.pack("d", time.time())
-
-    # Checksum ICMP packet using given function
-    checksum_val = checksum(header + data)
-
-    # Insert checksum into packet
-    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, socket.htons(checksum_val), ID, 1)
-
-    # Send packet using socket
-    icmpSocket.sendto(header + data, (destinationAddress, 1))
-
-    # Record time of sending
-    time_sent = time.time() * 1000  # Convert to milliseconds
-    return time_sent
-
-
-def doOnePing(destinationAddress, timeout):
-    try:
-        # Create ICMP socket
-        icmpSocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
-
-        # Generate a unique identifier for this ping
-        ID = os.getpid() & 0xFFFF
-
-        # Call sendOnePing function
-        time_sent = sendOnePing(icmpSocket, destinationAddress, ID)
-
-        # Call receiveOnePing function
-        response, time_received, ttl = receiveOnePing(icmpSocket, destinationAddress, ID, timeout)
-
-        # Close ICMP socket
-        icmpSocket.close()
-
-        if response is not None and time_received is not None:
-            # Calculate total network delay
-            network_delay = time_received - time_sent
-            return network_delay, ttl
-        else:
+def receiveoneping(icmp_socket, sent_time, timeout):
+    time_left = timeout
+    while True:
+        started_select = time.time()
+        what_ready = select.select([icmp_socket], [], [], time_left)
+        wait_time = (time.time() - started_select)
+        if what_ready[0] == []:  # Timeout
             return None, None
 
-    except socket.error as e:
-        print(f"Socket error: {e}")
-        return None, None
+        time_received = time.time()
+        rec_packet, addr = icmp_socket.recvfrom(1024)
+        icmp_header = rec_packet[20:28]
+        icmp_type, code, checksum, packet_id, sequence = struct.unpack(
+            "bbHHh", icmp_header
+        )
+
+        if packet_id < 0:
+            return None, None
+
+        time_left -= wait_time
+        if time_left <= 0:
+            return None, None
+
+        if icmp_type == ICMP_ECHO_REPLY:
+            return time_received - sent_time, addr
+
+        time_left = timeout
+
+
+
+def sendoneping(icmp_socket, dest_addr, packet_id):
+    checksum_value = 0
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, checksum_value, packet_id, 1)
+    data = struct.pack("d", time.time())
+    checksum_value = checksum(header + data)
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, socket.htons(checksum_value), packet_id, 1)
+    packet = header + data
+    icmp_socket.sendto(packet, (dest_addr, 1))
+
+
+def dooneping(dest_addr, timeout):
+    icmp = socket.getprotobyname("icmp")
+    icmp_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, icmp)
+    my_ID = os.getpid() & 0xFFFF
+    sendoneping(icmp_socket, dest_addr, my_ID)
+    delay, addr = receiveoneping(icmp_socket, time.time(), timeout)
+    icmp_socket.close()
+    return delay, addr
 
 
 def ping(host, timeout=1, count=4):
-    try:
-        # Look up hostname, resolving it to an IP address
-        destinationAddress = socket.gethostbyname(host)
-        print(f"Pinging {host} [{destinationAddress}] with 32 bytes of data:")
-
-        total_delay = 0
-        packets_sent = 0
-        packets_received = 0
-
-        for _ in range(count):
-            # Call doOnePing function, approximately every second
-            network_delay = doOnePing(destinationAddress, timeout)
-            network_delay, ttl = doOnePing(destinationAddress, timeout)
-            if network_delay is not None:
-                packets_sent += 1
-                packets_received += 1
-                total_delay += network_delay
-                print(f"Reply from {destinationAddress}: bytes=32 time={int(network_delay)}ms TTL={ttl}")
-                time.sleep(1)  # Wait for 1 second before sending the next ping
-            else:
-                print("Request timed out.")
-
-        if packets_sent > 0:
-            # Calculate packet loss and statistics
-            packet_loss = (packets_sent - packets_received) / packets_sent * 100
-            average_delay = total_delay / packets_received
-            print(f"\nPing statistics for {host}:")
-            print(
-                f"    Packets: Sent = {packets_sent}, Received = {packets_received}, Lost = {int(packet_loss)}% ({packets_sent - packets_received} lost),")
-            print(
-                f"Approximate round trip times in milliseconds:\n    Minimum = {int(average_delay)}ms, Maximum = {int(average_delay)}ms, Average = {int(average_delay)}ms")
-
-    except socket.gaierror as e:
-        print(f"Could not resolve hostname: {e}")
-    except KeyboardInterrupt:
-        print("\nPing terminated by user.")
+    dest_addr = socket.gethostbyname(host)
+    for i in range(count):
+        print(f"Pinging {host} [{dest_addr}] with 32 bytes of data:")
+        delay, addr = dooneping(dest_addr, timeout)
+        if delay is None:
+            print("Request timed out.")
+        else:
+            delay = round(delay * 1000, 2)
+            print(f"Reply from {addr}: bytes=32 time={delay}ms")
 
 
 # Example usage:
